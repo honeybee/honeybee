55bbb4054de224d2796753099217a77a01b1169e
Igor Pellegrini <igor.pellegrini@live.com>
2015-09-01 19:36:10 +0000

    Add 'collection' view translation domain
    - Generated taking (as convention) the first 3 parts of the view_scope

--- src/Ui/Renderer/Html/Honeybee/Projection/HtmlProjectionCollectionRenderer.php
+++ src/Ui/Renderer/Html/Honeybee/Projection/HtmlProjectionCollectionRenderer.php
@@ -6,6 +6,8 @@ use Honeybee\Ui\Renderer\EntityListRenderer;
 
 class HtmlProjectionCollectionRenderer extends EntityListRenderer
 {
+    const STATIC_TRANSLATION_PATH = "collection";
+
     protected function getTemplateParameters()
     {
         $params = parent::getTemplateParameters();
@@ -17,5 +19,26 @@ class HtmlProjectionCollectionRenderer extends EntityListRenderer
         $params['css'] = $css;
 
         return $params;
+    }
+
+    protected function getDefaultTranslationDomain()
+    {
+        $view_scope = $this->getOption('view_scope');
+
+        if (empty($view_scope)) {
+            $translation_domain_prefix = parent::getDefaultTranslationDomain();
+        } else {
+            // convention on view_scope value: the first 3 parts = vendor.package.resource_type
+            $view_scope_parts = explode('.', $view_scope);
+            $translation_domain_prefix = implode('.', array_slice($view_scope_parts, 0, 3));
+        }
+
+        $translation_domain = sprintf(
+            '%s.%s',
+            $translation_domain_prefix,
+            self::STATIC_TRANSLATION_PATH
+        );
+
+        return $translation_domain;
     }
 }
