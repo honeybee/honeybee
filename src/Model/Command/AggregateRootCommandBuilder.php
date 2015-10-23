85f3f2d52b54d7fb9b62da2136b125dc92dbb21a
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2015-10-23 10:25:05 +0000

    extracted sanitizeAttrbiteValue method

--- src/Model/Command/AggregateRootCommandBuilder.php
+++ src/Model/Command/AggregateRootCommandBuilder.php
@@ -6,11 +6,14 @@ use Honeybee\Infrastructure\Command\CommandBuilder;
 use Honeybee\Model\Aggregate\AggregateRootTypeInterface;
 use Shrink0r\Monatic\Error;
 use Shrink0r\Monatic\Success;
+use Trellis\Runtime\Attribute\AttributeInterface;
 use Trellis\Runtime\Validator\Result\IncidentInterface;
 
 class AggregateRootCommandBuilder extends CommandBuilder
 {
     protected $aggregate_root_type;
+
+    protected $embedded_builders;
 
     public function __construct(AggregateRootTypeInterface $aggregate_root_type, $command_class)
     {
@@ -18,6 +21,7 @@ class AggregateRootCommandBuilder extends CommandBuilder
 
         $this->aggregate_root_type = $aggregate_root_type;
         $this->command_state['aggregate_root_type'] = get_class($aggregate_root_type);
+        $this->embedded_builders = [];
     }
 
     protected function validateValues(array $values)
@@ -28,22 +32,37 @@ class AggregateRootCommandBuilder extends CommandBuilder
         foreach ($this->aggregate_root_type->getAttributes() as $attribute_name => $attribute) {
             $attribute = $this->aggregate_root_type->getAttribute($attribute_name);
             if (isset($values[$attribute_name])) {
-                $value_holder = $attribute->createValueHolder();
-                $result = $value_holder->setValue($values[$attribute_name]);
-                if ($result->getSeverity() > IncidentInterface::NOTICE) {
-                    $success = false;
-                    foreach ($result->getViolatedRules() as $rule) {
-                        foreach ($rule->getIncidents() as $name => $incident) {
-                            $error_key = $attribute->getPath() . '.' . $name;
-                            $incident_params = $incident->getParameters();
-                            $errors[$attribute_name] = [ $error_key, $incident_params ];
-                        }
-                    }
+                $result = $this->sanitizeAttributeValue($attribute, $values[$attribute_name]);
+                if ($result instanceof Success) {
+                    $sanitized_values[$attribute_name] = $result->get();
+                } else {
+                    $errors[$attribute_name] = $result->get();
                 }
-                $sanitized_values[$attribute_name] = $value_holder->toNative();
             }
         }
 
         return empty($errors) ? new Success($sanitized_values) : new Error($errors);
+    }
+
+    protected function sanitizeAttributeValue(AttributeInterface $attribute, $value)
+    {
+        $errors = [];
+        $sanitized_value = null;
+
+        $value_holder = $attribute->createValueHolder();
+        $result = $value_holder->setValue($value);
+
+        if ($result->getSeverity() > IncidentInterface::NOTICE) {
+            $success = false;
+            foreach ($result->getViolatedRules() as $rule) {
+                foreach ($rule->getIncidents() as $name => $incident) {
+                    $error_key = $attribute->getPath() . '.' . $name;
+                    $incident_params = $incident->getParameters();
+                    $errors[] = [ $error_key, $incident_params ];
+                }
+            }
+        }
+
+        return empty($errors) ? new Success($value_holder->toNative()) : new Error($errors);
     }
 }
