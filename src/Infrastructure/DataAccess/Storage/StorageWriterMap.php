8777112ff87b478cc769468b73bb3388cf821154
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2015-11-15 00:09:21 +0000

    convenient getter for StorageWriterMap

--- src/Infrastructure/DataAccess/Storage/StorageWriterMap.php
+++ src/Infrastructure/DataAccess/Storage/StorageWriterMap.php
@@ -2,11 +2,22 @@
 
 namespace Honeybee\Infrastructure\DataAccess\Storage;
 
+use Honeybee\Common\Error\RuntimeError;
 use Trellis\Common\Collection\TypedMap;
 use Trellis\Common\Collection\UniqueCollectionInterface;
 
 class StorageWriterMap extends TypedMap implements UniqueCollectionInterface
 {
+    public function getByEntityType($entity_type_prefix)
+    {
+        $writer_name = sprintf('%s::projection.standard::view_store::writer', $entity_type_prefix);
+        if (!$this->hasKey($writer_name)) {
+            throw new RuntimeError(sprintf('No storage-writer for key %s found.', $writer_name));
+        }
+
+        return $this->getItem($writer_name);
+    }
+
     protected function getItemImplementor()
     {
         return StorageWriterInterface::CLASS;
