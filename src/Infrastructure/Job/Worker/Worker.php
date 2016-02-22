e4bef57e77a2a515f421c9e2ef4590cea67ce6ae
Hasham Ahmad <mr.hash@gmail.com>
2016-02-22 13:10:59 +0000

    Support for async retry machanism

--- src/Infrastructure/Job/Worker/Worker.php
+++ src/Infrastructure/Job/Worker/Worker.php
@@ -9,11 +9,14 @@ use Honeybee\Infrastructure\Config\ConfigInterface;
 use Honeybee\Infrastructure\DataAccess\Connector\RabbitMqConnector;
 use Honeybee\Infrastructure\Job\JobInterface;
 use Honeybee\Infrastructure\Job\JobServiceInterface;
-use PhpAmqpLib\Exception\AMQPProtocolConnectionException;
 
 class Worker implements WorkerInterface
 {
-    const DEFAULT_RETRY_LIMIT = 3;
+    const DEFAULT_EXPIRATION = 5000;
+
+    const DEFAULT_EXPIRATION_MULTIPLIER = 3;
+
+    const DEFAULT_MAX_EXPIRATION = 86400000;
 
     protected $running = false;
 
@@ -23,14 +26,11 @@ class Worker implements WorkerInterface
 
     protected $job_service;
 
-    protected $retry_tracking;
-
     public function __construct(RabbitMqConnector $connector, ConfigInterface $config, JobServiceInterface $job_service)
     {
         $this->connector = $connector;
         $this->config = $config;
         $this->job_service = $job_service;
-        $retry_tracking = [];
     }
 
     public function run()
@@ -52,12 +52,20 @@ class Worker implements WorkerInterface
     {
         $exchange_name = $this->config->get('exchange');
         $queue_name = $this->config->get('queue');
+        $wait_exchange_name = $this->config->get('wait_exchange');
+        $wait_queue_name = $this->config->get('wait_queue');
 
         if (!$exchange_name) {
-            throw new RuntimeError("Missing required 'exchange_name' config setting.");
+            throw new RuntimeError("Missing required 'exchange' config setting.");
         }
         if (!$queue_name) {
-            throw new RuntimeError("Missing required 'queue_name' config setting.");
+            throw new RuntimeError("Missing required 'queue' config setting.");
+        }
+        if (!$wait_exchange_name) {
+            throw new RuntimeError("Missing required 'wait_exchange' config setting.");
+        }
+        if (!$wait_queue_name) {
+            throw new RuntimeError("Missing required 'wait_queue' config setting.");
         }
     }
 
@@ -65,18 +73,27 @@ class Worker implements WorkerInterface
     {
         $exchange_name = $this->config->get('exchange');
         $queue_name = $this->config->get('queue');
+        $wait_exchange_name = $this->config->get('wait_exchange');
+        $wait_queue_name = $this->config->get('wait_queue');
         $channel = $this->connector->getConnection()->channel();
 
         $channel->basic_qos(null, 1, null);
         $channel->exchange_declare($exchange_name, 'direct', false, true, false);
         $channel->queue_declare($queue_name, false, true, false, false);
+        $channel->exchange_declare($wait_exchange_name, 'direct', false, true, false);
+        $channel->queue_declare($wait_queue_name, false, true, false, false, false, [
+            'x-dead-letter-exchange' => [ 'S', $exchange_name ]
+        ]);
 
+        // bind routing keys to both exchange and wait_exchange to enable DLX retry
         $bindings = (array)$this->config->get('bindings', []);
         if (empty($bindings)) {
             $channel->queue_bind($queue_name, $exchange_name, 'default');
+            $channel->queue_bind($wait_queue_name, $wait_exchange_name, 'default');
         } else {
             foreach ($bindings as $binding) {
                 $channel->queue_bind($queue_name, $exchange_name, $binding);
+                $channel->queue_bind($wait_queue_name, $wait_exchange_name, $binding);
             }
         }
 
@@ -90,50 +107,51 @@ class Worker implements WorkerInterface
 
     protected function onJobScheduledForExecution($job_message)
     {
-        $job_failed = true;
-
         try {
+            $delivery_info = $job_message->delivery_info;
+            $channel = $delivery_info['channel'];
+            $delivery_tag = $delivery_info['delivery_tag'];
             $job = $this->job_service->createJob(JsonToolkit::parse($job_message->body));
             $job->run();
-            $job_failed = false;
         } catch (Exception $runtime_error) {
-            throw $runtime_error;
-            $jid = $job->getUuid();
-            $job_success = false;
-            if (!isset($this->retry_tracking[$jid])) {
-                $this->retry_tracking[$jid] = 0;
-            }
-            $this->retry_tracking[$jid]++;
             // @todo appropiate error-logging
             error_log(__METHOD__ . ' - ' . $runtime_error->getMessage() . PHP_EOL . $runtime_error->getMessage());
             printf(
                 "\n\n[Worker] Unexpected error during execution of job(id) '%s' with message %s and trace:\n%s\n\n",
-                $jid,
+                $job->getUuid(),
                 $runtime_error->getMessage(),
                 $runtime_error->getTraceAsString()
             );
+
+            // republish failed message to wait exchange with new expiration time
+            $job_message->set('expiration', $this->getExpirationIntervalFor($job_message));
+            $channel->basic_publish(
+                $job_message,
+                $this->config->get('wait_exchange'),
+                $job_message->get('routing_key')
+            );
+
+            //@todo handle failed message by publishing a new event
         }
 
-        $delivery_info = $job_message->delivery_info;
-        $channel = $delivery_info['channel'];
-        $delivery_tag = $delivery_info['delivery_tag'];
-        if ($job_failed) {
-            if ($this->retry_tracking[$jid] <= $this->getMaxRetriesAllowedFor($job)) {
-                $channel->basic_reject($delivery_tag, true);
-            } else {
-                // @todo the job is now dropped from queue as fatal.
-                // we might want to push it to an error queue or to a journal/recovery file for fatal jobs.
-                unset($this->retry_tracking[$jid]);
-                $channel->basic_reject($delivery_tag, false);
-            }
-        } else {
-            $channel->basic_ack($delivery_tag);
-        }
+        // acknowledge the message to remove it from the event queue. An 'ack' is effectively the
+        // same as a 'nack'.
+        $channel->basic_ack($delivery_tag);
     }
 
-    protected function getMaxRetriesAllowedFor(JobInterface $job)
+    protected function getExpirationIntervalFor($job_message)
     {
-        // @todo make this configurable based on metrics like job-type, -frequence etc.
-        return self::DEFAULT_RETRY_LIMIT;
+        $expiration = self::DEFAULT_EXPIRATION;
+        if ($job_message->has('application_headers')) {
+            $headers = $job_message->get('application_headers')->getNativeData();
+            //@note the size of the x-death array is the number of attempts for the message
+            if (isset($headers['x-death'][0]['original-expiration'])) {
+                $expiration = min(
+                    $headers['x-death'][0]['original-expiration'] * self::DEFAULT_EXPIRATION_MULTIPLIER,
+                    self::DEFAULT_MAX_EXPIRATION
+                );
+            }
+        }
+        return (int)$expiration;
     }
 }
