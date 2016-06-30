c7efcf17f79d29f6a3487a6c556981d0aa3313a9
Igor Pellegrini <igor.pellegrini@live.com>
2016-06-30 15:38:10 +0000

    Reintroduce ResourceCollection

---
+++ src/Ui/ResourceCollection.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Honeybee\Ui;
+
+use Trellis\Common\Collection\Collection;
+use Trellis\Common\Collection\CollectionInterface;
+
+class ResourceCollection extends Collection
+{
+    // CollectionInterface
+
+    public function filter(Closure $callback)
+    {
+        $filtered_list = new static();
+
+        foreach ($this->items as $item) {
+            if ($callback($item) === true) {
+                $filtered_list->push($item);
+            }
+        }
+
+        return $filtered_list;
+    }
+
+    public function append(CollectionInterface $collection)
+    {
+        if (!$collection instanceof static) {
+            throw new RuntimeException(
+                sprintf("Can only append collections of the same type %s", get_class($this))
+            );
+        }
+
+        foreach ($collection as $item) {
+            $this->addItem($item);
+        }
+    }
+}
