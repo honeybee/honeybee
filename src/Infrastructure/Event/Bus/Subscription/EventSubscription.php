940ee05c52cddf75054cc8c97a3c5805d34d178e
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2017-03-13 16:33:21 +0100

    add convenience method for retrieving transport, cleanup event-strategy prop

--- src/Infrastructure/Event/Bus/Subscription/EventSubscription.php
+++ src/Infrastructure/Event/Bus/Subscription/EventSubscription.php
@@ -16,8 +16,6 @@ class EventSubscription extends Object implements EventSubscriptionInterface
 
     protected $event_handlers;
 
-    protected $event_strategy;
-
     protected $settings;
 
     protected $activated;
@@ -26,14 +24,12 @@ class EventSubscription extends Object implements EventSubscriptionInterface
         EventTransportInterface $event_transport,
         EventFilterList $event_filters,
         EventHandlerList $event_handlers,
-        EventStrategy $event_strategy,
         SettingsInterface $settings,
         $activated
     ) {
         $this->event_transport = $event_transport;
         $this->event_handlers = $event_handlers;
         $this->event_filters = $event_filters;
-        $this->event_strategy = $event_strategy;
         $this->settings = $settings;
         $this->activated = (bool)$activated;
     }
@@ -55,7 +51,7 @@ class EventSubscription extends Object implements EventSubscriptionInterface
 
     public function getEventStrategy()
     {
-        return $this->event_strategy;
+        return null;
     }
 
     public function getSettings()
--- src/Infrastructure/Event/Bus/Subscription/EventSubscriptionList.php
+++ src/Infrastructure/Event/Bus/Subscription/EventSubscriptionList.php
@@ -7,6 +7,16 @@ use Trellis\Common\Collection\TypedList;
 
 class EventSubscriptionList extends TypedList implements UniqueValueInterface
 {
+    public function getTransportByName($transport_name)
+    {
+        foreach ($this->items as $sub) {
+            if ($sub->getEventTransport()->getName() === $transport_name) {
+                return $sub->getEventTransport();
+            }
+        }
+        return null;
+    }
+
     protected function getItemImplementor()
     {
         return EventSubscriptionInterface::CLASS;
