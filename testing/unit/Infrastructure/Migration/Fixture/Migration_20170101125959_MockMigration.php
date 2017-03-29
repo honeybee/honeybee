e2a624466087eca88ef7da9b9e733d85c8fd8f53
Hasham Ahmad <mr.hash@gmail.com>
2017-03-29 12:11:03 +0100

    Update migration loader and test

---
+++ testing/unit/Infrastructure/Migration/Fixture/Migration_20170101125959_MockMigration.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Honeybee\Tests\Infrastructure\Migration\Fixture;
+
+use Honeybee\Infrastructure\Migration\Migration;
+use Honeybee\Infrastructure\Migration\MigrationTargetInterface;
+
+// @codingStandardsIgnoreLine
+class Migration_20170101125959_MockMigration extends Migration
+{
+    protected function up(MigrationTargetInterface $migration_target)
+    {
+    }
+
+    protected function down(MigrationTargetInterface $migration_target)
+    {
+    }
+
+    public function getDescription($direction = self::MIGRATE_UP)
+    {
+        if ($direction === MigrationInterface::MIGRATE_UP) {
+            return 'Upward migration fixture';
+        }
+        return 'Downward migration fixture';
+    }
+
+    public function isReversible()
+    {
+        return true;
+    }
+}
